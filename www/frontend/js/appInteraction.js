f9ec3bb8512a4d4490f8e9178b51882d2130a81b
Bo Simonsen <bo@geekworld.dk>
2014-09-27 15:22:10 +0200

    Added getClosestBeacon

--- www/frontend/js/appInteraction.js
+++ www/frontend/js/appInteraction.js
@@ -1,5 +1,8 @@
 var appInteraction = {
     "showAndroidToast": function() {
         Android.showToast();
+    },
+    "getClosestBeacon": function() {
+        return Android.getClosestBeacon();
     }
-}
+};
